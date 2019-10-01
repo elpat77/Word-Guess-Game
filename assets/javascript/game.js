d0fbfce99a7c98b41c36c04e69bd7f25d5c8961f
elpat7 <aguilarpat7@gmail.com>
2019-10-01 15:40:18 -0700

    adding logic to break the random word into letters, comapring user input whith those letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,35 @@
 var wins = 0;
 var currentWord = [];
 var guessesRemaining = 7;
-var lettersGuessed = [];
-var animalSelected = [];
-var animals = ["turtle", "parrot", "dolphin", "elephant", "girraffe", "eagle"];
-
+var guesses = [];
+var currentAnimal = [];
+var animals = ["turtle", "parrot", "dolphin", "elephant", "giraffe", "eagle"];
+var animalLetters = [];
 
 console.log(animals);
 
 function updateAnimalSelected() {
-    animalSelected = animals[Math.floor(Math.random() * animals.length)];
+    currentAnimal = animals[Math.floor(Math.random() * animals.length)];
     console.log("Starting new game");
-    console.log("The animal is " + animalSelected);
+    console.log("The animal is " + currentAnimal);
 }
+
 updateAnimalSelected();
-console.log(animalSelected);
+console.log(currentAnimal);
+
+var animalLetters = currentAnimal.split('');
+console.log(animalLetters);
+
+function updateGuessed() {
+    document.querySelector("#guessed").innerHTML = "Letters guessed so Far: " + guesses;
+}
+
+updateGuessed();
+
+document.onkeyup = function (event) {
+    if (animalLetters.includes(event.key)) {
+        console.log("Correct letter " + event.key);
+    } else {
+        console.log("Wrong Letter " + event.key);
+    }
+}
